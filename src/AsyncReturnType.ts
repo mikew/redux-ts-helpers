c106a1fe8d810c29531126e61bd714aa4576191e
Mike Wyatt <wyatt.mike@gmail.com>
2018-05-05 17:35:49 -0300

    lint

--- src/AsyncReturnType.ts
+++ src/AsyncReturnType.ts
@@ -1,10 +1,2 @@
 export type AsyncReturnType<T> =
   T extends (...args: any[]) => Promise<infer U> ? U : T
-
-async function foo() {
-  return { bar: 42, baz: 'world' }
-}
-
-function reducer(state: any, payload: AsyncReturnType<typeof foo>) {
-
-}
