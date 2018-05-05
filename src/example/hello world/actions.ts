e798ab9d13af15f0e609d47e8dfda99e50473705
Mike Wyatt <wyatt.mike@gmail.com>
2018-05-05 17:36:07 -0300

    add example

---
+++ src/example/hello world/actions.ts
@@ -0,0 +1,30 @@
+import {
+  applyNamespace,
+  createAction,
+} from '../../index'
+
+export const constants = applyNamespace('example', {
+  increment: 0,
+  setIncrementBy: 0,
+  promiseAction: 0,
+  asyncAction: 0,
+})
+
+export const increment = createAction(constants.increment)
+export const setIncrementBy = createAction<number>(constants.setIncrementBy)
+
+export const promiseAction = (id: number) => ({
+  type: constants.promiseAction,
+  payload: someApiCall(id),
+})
+
+export const asyncAction = (id: number) => ({
+  type: constants.asyncAction,
+  async payload() {
+    return someApiCall(id)
+  },
+})
+
+function someApiCall(id: number) {
+  return Promise.resolve({ id, name: 'hello world' })
+}
---
+++ src/example/hello world/reducer_withTypes.ts
@@ -0,0 +1,55 @@
+import {
+  AsyncReturnType,
+  createReducer,
+  PromiseType,
+} from '../../index'
+
+import * as actions from './actions'
+
+export const initialState = {
+  currentValue: 0,
+  incrementBy: 0,
+  promiseResult: '',
+  asyncResult: '',
+}
+
+export const reducer = createReducer(initialState, {
+  [actions.constants.setIncrementBy]: (
+    state,
+    action: ReturnType<typeof actions.setIncrementBy>,
+  ) => {
+    return {
+      ...state,
+      incrementBy: action.payload,
+    }
+  },
+
+  [actions.constants.increment]: (state) => ({
+    ...state,
+    currentValue: state.currentValue + state.incrementBy,
+  }),
+
+  [`${actions.constants.promiseAction}/success`]: (
+    state,
+    action: ReturnType<typeof actions.promiseAction>,
+  ) => {
+    const payload = action.payload as any as PromiseType<typeof action.payload>
+
+    return {
+      ...state,
+      promiseResult: payload.name,
+    }
+  },
+
+  [`${actions.constants.asyncAction}/success`]: (
+    state,
+    action: ReturnType<typeof actions.asyncAction>,
+  ) => {
+    const payload = action.payload as any as AsyncReturnType<typeof action.payload>
+
+    return {
+      ...state,
+      promiseResult: payload.name,
+    }
+  },
+})
