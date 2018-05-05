da6c03e42cb45e1dae90e30cee76aabbf764b92d
Mike Wyatt <wyatt.mike@gmail.com>
2018-05-05 17:34:12 -0300

    avoid annoying gotchas with newer versions of typescript
    - "Exported variable 'X' has or is using name 'Y' from external module" https://github.com/Microsoft/TypeScript/issues/5711
    - using new Types causes weird inferrence issues in createReducer

--- src/createAction.ts
+++ src/createAction.ts
@@ -2,16 +2,23 @@
  * Factory function that returns a redux action creator with two arguments:
  * - the first is the payload
  * - the second is the meta
- * @export
- * @template TPayload
- * @template TMeta
- * @param {string} type
- * @returns {(payload: TPayload, meta?: TMeta) => Action}
  */
 export default function createAction<TPayload = any, TMeta = any>(type: string) {
   return (payload: TPayload, meta?: TMeta) => ({
     type,
     payload,
     meta,
+  } as {
+    type: string
+    payload: TPayload
+    // This whole `as {}` bit is needed with recent versions of TypeScript,
+    // there's a bug somewhere but finding it is a rabbit hole of:
+    // - importing FluxAction, which then means people using `createAction` will
+    //   be hit by https://github.com/Microsoft/TypeScript/issues/5711
+    // - TypeScript not being able to infer that `meta` here is properly
+    //   optional, and not just `any | undefined` (which yes, is exactly how
+    //   optional appears yet "not technically" optional)
+    // tslint:disable-next-line:trailing-comma
+    meta?: TMeta
   })
 }
--- src/createReducer.ts
+++ src/createReducer.ts
@@ -1,19 +1,22 @@
-import { AnyAction, Reducer } from 'redux'
+import { Reducer } from 'redux'
 
 export interface HandlerMap<T> {
-  [actionName: string]: (state: T, action: AnyAction) => T
+  [actionName: string]: (state: T, action: {
+    type: string
+    payload: any
+    // tslint:disable-next-line:trailing-comma
+    meta?: any
+  }) => T
 }
 
 /**
  * Helper to create a reducer.
- * @export
- * @template T
- * @param {T} initialState
- * @param {HandlerMap<T>} handlers
- * @returns {Reducer<T>}
  */
-export default function createReducer<T>(initialState: T, handlers: HandlerMap<T>): Reducer<T> {
-  return (state: T = initialState, action: AnyAction) => {
+export default function createReducer<T>(
+  initialState: T,
+  handlers: HandlerMap<T>,
+): Reducer<T, { type: string, payload: any, meta?: any }> {
+  return (state: T = initialState, action: { type: string, payload: any, meta?: any }) => {
     if (!handlers[action.type]) {
       return state
     }
