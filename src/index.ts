1b7000d18b76747253371e3943248b0e7c8f4b1b
Mike Wyatt <wyatt.mike@gmail.com>
2018-05-05 17:36:34 -0300

    add more exports to main index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 export { default as applyNamespace } from './applyNamespace'
 export { default as createAction } from './createAction'
 export { default as createReducer } from './createReducer'
+
 export { default as tsGetReturnType } from './tsGetReturnType'
 export { default as tsGetFirstArgType } from './tsGetFirstArgType'
 export { default as tsGetPromiseType } from './tsGetPromiseType'
+export { default as tsGetAsyncReturnType } from './tsGetAsyncReturnType'
+
+export { PromiseType } from './PromiseType'
+export { AsyncReturnType } from './AsyncReturnType'
