188b0909d0e73c66aebd1a1bab7e6bdcd886d559
Mike Wyatt <wyatt.mike@gmail.com>
2018-12-25 23:24:54 -0330

    fix issues with strictFunctionTypes

--- src/createReducer.ts
+++ src/createReducer.ts
@@ -1,17 +1,22 @@
-import { AnyAction, Reducer } from 'redux'
+import { Action, Reducer } from 'redux'
 
-export interface HandlerMap<T> {
-  [actionName: string]: (state: T, action: AnyAction) => T
+export interface HandlerMap<S, A extends Action = any> {
+  [actionName: string]: (state: S, action: A) => S
 }
 
 /**
  * Helper to create a reducer.
  */
-export default function createReducer<T>(
-  initialState: T,
-  handlers: HandlerMap<T>,
-): Reducer<T> {
-  return (state: T = initialState, action) => {
+// TODO The default is `any`, otherwise there's conflicts with casting an action
+// to `ReturnType<typeof actions.whatever>` in a reducer when
+// `strictFunctionTypes` is enabled.
+// Explicitly don't infer the `Action` type, otherwise things get real
+// complicated.
+export default function createReducer<S>(
+  initialState: S,
+  handlers: HandlerMap<S, any>,
+): Reducer<S, any> {
+  return (state: S = initialState, action) => {
     if (!handlers[action.type]) {
       return state
     }
