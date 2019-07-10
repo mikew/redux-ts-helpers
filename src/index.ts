c2a055468a15fe1a3171178db33bf63c0ad28753
Mike Wyatt <wyatt.mike@gmail.com>
2019-07-10 10:59:54 -0300

    export wrapWithMeta in main file

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as applyNamespace } from './applyNamespace'
 export { default as createAction } from './createAction'
 export { default as createReducer } from './createReducer'
+export { default as wrapWithMeta } from './wrapWithMeta'
